7e6dead58395fbf4deff812b1e0900e944c982ac
jeley0 <91151123+jeley0@users.noreply.github.com>
2023-03-05 20:40:26 +0700

    Create lab2.5.py

---
+++ Modern_applications/Lab2/lab2.5.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("nums", nargs='*')
+args = parser.parse_args()
+
+if len(args.nums) == 0:
+    print('NO PARAMS')
+elif len(args.nums) < 2:
+    print('TOO FEW PARAMS')
+elif len(args.nums) > 2:
+    print('TOO MANY PARAMS')
+else:
+    try:
+        nums = list(map(int, args.nums))
+        print(sum(nums))
+    except Exception as e:
+        print(type(e).__name__)
