26a905cb9138b9081efde8a627a363120e3b533b
jeley0 <91151123+jeley0@users.noreply.github.com>
2023-03-05 20:21:34 +0700

    Create lab2.3.py

---
+++ Modern_applications/Lab2/lab2.3.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("any_args", nargs="*")
+
+args = parser.parse_args()
+
+if args.any_args:
+    for i in args.any_args:
+        print(i)
+else:
+    print("no args")
